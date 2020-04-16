19dbd816a3ace18213481ed8af960d9c763b687a
Gutapa <chmgumgu@gmail.com>
2020-04-17 03:49:53 +0530

    Added event handling to Quit when we click X

--- pygame1st.py
+++ pygame1st.py
@@ -1,4 +1,20 @@
 import pygame
-
+pygame.init()
 win = pygame.display.set_mode((1800,1200))
 pygame.display.set_caption("First Game")
+
+x=50
+y=50
+width=40
+height=60
+vel=5
+
+run=True
+while run:
+	pygame.time.delay(100)
+
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			run=False
+
+pygame.quit()
